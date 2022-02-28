78fc63d442edb943519b5c55d1b6c838079c463e
UP851899 <72556275+UP851899@users.noreply.github.com>
2022-02-28 13:56:10 +0000

    dns server example

---
+++ dns-server.js
@@ -0,0 +1,44 @@
+const dns2 = require('dns2');
+
+const { Packet } = dns2;
+
+const server = dns2.createServer({
+    udp: true,
+    handle: (request, send, rinfo) => {
+        const response = Packet.createResponseFromRequest(request);
+        const [question] = request.questions;
+        const { name } = question;
+        response.answers.push({
+            name,
+            type: Packet.TYPE.A,
+            class: Packet.CLASS.IN,
+            ttl: 300,
+            address: '8.8.8.8'
+        });
+        send(response);
+    }
+});
+
+server.on('request', (request, response, rinfo) => {
+    console.log(request.header.id, request.questions[0]);
+});
+
+server.on('requestError', (error) => {
+    console.log('Client sent an invalid request', error);
+});
+
+server.on('listening', () => {
+    console.log(server.addresses());
+});
+
+server.on('close', () => {
+    console.log('server closed');
+});
+
+server.listen({
+    // Optionally specify port and/or address for each server:
+    udp: { port: 5333 }
+});
+
+// eventually
+server.close();
