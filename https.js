7fc3cb49bf5788d2fb79a8a4bad9aee76b02b8a1
UP851899 <72556275+UP851899@users.noreply.github.com>
2022-03-08 22:22:04 +0000

    example

--- https.js
+++ https.js
@@ -1,43 +1,73 @@
-var fs = require('fs'),
-    http = require('http'),
-    https = require('https'),
-    httpProxy = require('http-proxy');
+const httpProxy = require("http-proxy"),
+    http = require("http"),
+    url = require("url"),
+    net = require('net'),
+    port = 443, //Port for proxy running on this machines local IP
+    hostIP = '0.0.0.0',
+    expressPort = 8080;
 
-var isHttps = true; // do you want a https proxy?
+const server = http.createServer(function (req, res) {
+    let urlObj = url.parse(req.url);
+    let target = urlObj.protocol + "//" + urlObj.host;
 
-var options = {
-    https: {
-        key: fs.readFileSync('key.pem'),
-        cert: fs.readFileSync('cert.pem')
+    console.log("HTTP request for:", target);
+
+    const proxy = httpProxy.createProxyServer({});
+    proxy.on("error", function (err, req, res) {
+        console.log("proxy error", err);
+        res.end();
+    });
+
+    proxy.web(req, res, { target: target });
+})
+
+const regexForPort = /^([^:]+)(:([0-9]+))?$/;
+
+const getPortFromURL = function (hostString, defaultPort) {
+    let host = hostString;
+    let port = defaultPort;
+
+    let result = regexForPort.exec(host);
+    if (result != null) {
+        host = result[1];
+        if (result[2] != null) {
+            port = result[3];
+        }
     }
+
+    return ([host, port]);
 };
 
-// this is the target server
-var proxy = new httpProxy.createProxyServer({
-    target: {
-        host: '0.0.0.0',
-        port: 8080
+server.addListener('connect', function (req, socket, bodyhead) {
+    let hostPort = getPortFromURL(req.url, 443);
+    let hostDomain = hostPort[0];
+    let port = parseInt(hostPort[1]);
+    console.log("HTTPS request for:", hostDomain, port);
+
+    let proxySocket = new net.Socket();
+    proxySocket.connect(port, hostDomain, function () {
+        proxySocket.write(bodyhead);
+        socket.write("HTTP/" + req.httpVersion + " 200 Connection established\r\n\r\n");
     }
+    );
+
+    proxySocket.on('data', function (chunk) {
+        socket.write(chunk);
+    });
+
+    proxySocket.on('end', function () {
+        socket.end();
+    });
+
+    socket.on('data', function (chunk) {
+        proxySocket.write(chunk);
+    });
+
+    socket.on('end', function () {
+        proxySocket.end();
+    });
 });
 
-if (isHttps)
-    https.createServer(options.https, function (req, res) {
-        console.log('Proxying https request at %s', new Date());
-        proxy.proxyRequest(req, res);
-    }).listen(443, function (err) {
-        if (err)
-            console.log('Error serving https proxy request: %s', req);
-
-        // console.log('Created https proxy. Forwarding requests from %s to %s:%s', '443', req.host, req.port);
-    });
-else
-    http.createServer(options.https, function (req, res) {
-        console.log('Proxying http request at %s', new Date());
-        console.log(req);
-        proxy.proxyRequest(req, res);
-    }).listen(80, function (err) {
-        if (err)
-            console.log('Error serving http proxy request: %s', req);
-
-        // console.log('Created http proxy. Forwarding requests from %s to %s:%s', '80', req.host, req.port);
-    });
+server.listen(port, hostIP, () => {
+    console.log("Proxy running of port 8080");
+});  //this is the port your clients will connect to
