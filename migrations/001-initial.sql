c743326f894f2a76c8b458b4080a103b5804ff1c
UP851899 <72556275+UP851899@users.noreply.github.com>
2022-03-10 15:43:42 +0000

    initialise db for websites and filter

---
+++ migrations/001-initial.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS blockedSites;
+DROP TABLE IF EXISTS websiteFilters;
+
+-- Create blocked sites --
+CREATE TABLE websiteFilters (
+    filter_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    filter_name TEXT NOT NULL,
+);
+
+CREATE TABLE blockedSites (
+    site_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    site_address TEXT NOT NULL,
+    filter_ID INTEGER NOT NULL,
+    FOREIGN KEY (filter_ID)
+        REFERENCES websiteFilters (filter_ID)
+);
