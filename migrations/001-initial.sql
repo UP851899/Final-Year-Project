880b3c392dd9d9926e7503ccdbfb58ec07edd29f
UP851899 <72556275+UP851899@users.noreply.github.com>
2022-03-17 17:04:13 +0000

    Added initial DB values

--- migrations/001-initial.sql
+++ migrations/001-initial.sql
@@ -1,6 +1,3 @@
-DROP TABLE IF EXISTS blockedSites;
-DROP TABLE IF EXISTS websiteFilters;
-
 CREATE TABLE websiteFilters (
     filter_ID INTEGER PRIMARY KEY AUTOINCREMENT,
     filter_name TEXT NOT NULL
@@ -13,3 +10,32 @@ CREATE TABLE blockedSites (
     FOREIGN KEY (filter_ID)
         REFERENCES websiteFilters (filter_ID)
 );
+
+-- Insert Default filters --
+INSERT INTO websiteFilters (filter_name)
+VALUES
+    ("Gambling"),
+    ("Social Media"),
+    ("Adult content"),
+    ("News"),
+    ("Drugs"),
+    ("Shopping"),
+    ("Entertainment"),
+    ("Forum"),
+    ("Games"),
+    ("Virus & Malware");
+
+-- Insert Default Blocking Sites/Keywords --
+INSERT INTO blockedSites (site_address, filter_ID)
+VALUES
+    ("Reddit", 2),
+    ("Omegle.com", 3),
+    ("Tinder", 3),
+    ("Paddypower.com", 1),
+    ("Chatroulette.com", 3),
+    ("4Chan", 3),
+    ("ebay.com", 6),
+    ("Amazon.com", 6);
+
+
+    
